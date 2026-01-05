bdf55874a39e3ddb76b5281a5e17373e88a647fb
Hisantu <santoshsantu92067@gmail.com>
2026-01-05 11:29:29 +0530

    Configure single service deployment for Render

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const path = require('path');
 const dotenv = require('dotenv');
 dotenv.config();
 require('./config/db')(); // connect to MongoDB
@@ -12,6 +13,9 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(morgan('dev'));
 
+// Serve static files from React build
+app.use(express.static(path.join(__dirname, '../../web/dist')));
+
 // Routes
 app.get('/api/health', (req, res) => res.json({ ok: true, now: new Date() }));
 
@@ -21,6 +25,11 @@ app.use('/api/tokens', require('./routes/tokens'));
 app.use('/api/appointments', require('./routes/appointments'));
 app.use('/api/doctors', require('./routes/doctors'));
 app.use('/api/display', require('./routes/display'));
+
+// Serve React app for all non-API routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../web/dist/index.html'));
+});
 
 // error handler
 app.use((err, req, res, next) => {
